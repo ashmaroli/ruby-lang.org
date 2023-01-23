0c99f4bad2cbca72b680d19217ebad2e5dc555de
Ashwin Maroli <ashmaroli@gmail.com>
2023-01-23 14:22:39 +0530

    Profile memory usage

---
+++ .github/actions/memprof.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'jekyll'
+require 'memory_profiler'
+
+MemoryProfiler.report(allow_files: ['lib/jekyll/', 'lib/jekyll.rb']) do
+  Jekyll::Commands::Build.process({
+    "source"             => File.expand_path("../../", __dir__),
+    "destination"        => File.expand_path("../../_site", __dir__),
+    "disable_disk_cache" => true,
+  })
+  puts ''
+end.pretty_print(scale_bytes: true, normalize_paths: true)
